9882868c97debef86edb436a0e0c2d26320a5a8e
akkuankka <cwmckenzie02@gmail.com>
2021-11-18 22:35:53 +1030

    Added networking constants

---
+++ src/main/kotlin/es/headbe/texnes/util/networking/NetworkingConstants.kt
@@ -0,0 +1,8 @@
+package es.headbe.texnes.util.networking
+
+import es.headbe.texnes.util.ident
+import net.minecraft.util.Identifier
+
+object NetworkingConstants {
+    val PRESSURE_GAUGE_PKID = ident("packet_open_pressure_gauge")
+}
