14968c19462fc6d082b985165cb834469333e570
akkuankka <cwmckenzie02@gmail.com>
2021-11-18 22:36:20 +1030

    Added client

---
+++ src/main/kotlin/es/headbe/texnes/TexnesClient.kt
@@ -0,0 +1,18 @@
+package es.headbe.texnes
+
+import es.headbe.texnes.gui.PressureGaugeGui
+import es.headbe.texnes.gui.PressureGaugeScreen
+import es.headbe.texnes.util.networking.NetworkingConstants
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+
+@Suppress("unused")
+class TexnesClient {
+    fun init() {
+        ClientPlayNetworking.registerGlobalReceiver(NetworkingConstants.PRESSURE_GAUGE_PKID)
+        {client, handler, buf, responseSender ->
+            client.execute {
+                client.setScreen(PressureGaugeScreen(PressureGaugeGui()))
+            }
+        }
+    }
+}
