cdb72afc92d48312784fad4fefe3e7af88f299b6
akkuankka <cwmckenzie02@gmail.com>
2021-11-18 22:36:44 +1030

    Changed Pressure gauge rendering system to not crash!

--- src/main/kotlin/es/headbe/texnes/gui/PressureGaugeGui.kt
+++ src/main/kotlin/es/headbe/texnes/gui/PressureGaugeGui.kt
@@ -2,12 +2,18 @@ package es.headbe.texnes.gui
 
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription
 import io.github.cottonmc.cotton.gui.widget.WGridPanel
+import io.github.cottonmc.cotton.gui.widget.WLabel
 
 class PressureGaugeGui : LightweightGuiDescription() {
     init {
-        val root = WGridPanel()
+        var root = WGridPanel(1)
         setRootPanel(root)
         root.setSize(300, 200)
+
+        val label = WLabel("Pressure is over 9000")
+        root.add(label, 1, 1)
+
+        root.validate(this)
     }
 
 }
--- src/main/kotlin/es/headbe/texnes/registry/items/PressureGauge.kt
+++ src/main/kotlin/es/headbe/texnes/registry/items/PressureGauge.kt
@@ -2,10 +2,15 @@ package es.headbe.texnes.registry.items
 
 import es.headbe.texnes.gui.PressureGaugeGui
 import es.headbe.texnes.gui.PressureGaugeScreen
+import es.headbe.texnes.util.networking.NetworkingConstants
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking
 import net.minecraft.client.MinecraftClient
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.network.PacketByteBuf
+import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
@@ -13,7 +18,13 @@ import net.minecraft.world.World
 class PressureGauge(settings: Settings) : Item(settings) {
 
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-        MinecraftClient.getInstance().setScreen(PressureGaugeScreen(PressureGaugeGui()))
-        return super.use(world, user, hand)
+        if (world.isClient()) return super.use(world, user, hand)
+
+        // otherwise we're on the server
+
+        // TODO get the pressure of the pipe that's being looked at
+        ServerPlayNetworking.send(user as ServerPlayerEntity, NetworkingConstants.PRESSURE_GAUGE_PKID, PacketByteBufs.empty())
+//        MinecraftClient.getInstance().setScreen(PressureGaugeScreen(PressureGaugeGui()))
+        return TypedActionResult.success(user.getStackInHand(hand))
     }
 }
