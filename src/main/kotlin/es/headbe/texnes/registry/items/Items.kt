2c063af5c5cea975804c489b1e4dd37defcfbaf5
akkuankka <cwmckenzie02@gmail.com>
2022-03-20 16:53:57 +1030

    Datagen lead

--- src/main/kotlin/es/headbe/texnes/registry/items/Items.kt
+++ src/main/kotlin/es/headbe/texnes/registry/items/Items.kt
@@ -3,6 +3,7 @@ package es.headbe.texnes.registry.items
 import es.headbe.texnes.Texnes
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import es.headbe.texnes.util.*
+import net.minecraft.item.Item
 
 object Items {
 // normal items
@@ -14,10 +15,15 @@ object Items {
         FabricItemSettings().group(Texnes.ITEM_GROUP)
     )
 
+
+    fun genericItem(): Item = Item(FabricItemSettings().group(Texnes.ITEM_GROUP))
+
     fun registerAll() {
         // item items
         ident("bismuth").item(bismuth)
         ident("pressure_gauge").item(pressureGauge)
+        ident("lead_ingot").item(genericItem())
+        ident("lead_plate").item(genericItem())
     }
 }
 
